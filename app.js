c551720a8b324cb5423bcfa008ac89389ce07a29
Ayushman <ayushman@honeybadgerlabs.in>
2020-07-21 14:50:41 +0530

    addded app.js cors setting

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var userapi = require('./userCall');
 
+
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
@@ -18,6 +32,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(allowCrossDomain);
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', (req, res) => {
