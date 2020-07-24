4c94fc24d2666e1e46f54ca2c6cc5032dede8d22
Ayushman <ayushman@honeybadgerlabs.in>
2020-07-24 20:17:07 +0530

    need to remove keys

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ var config = {}
 
 config.twitter = {
     'apiUrl': 'https://api.twitter.com/1.1/',
-    'key': 'UZAMY9RYrVWOAbRDzjMecTvYP',
     'token': 'AAAAAAAAAAAAAAAAAAAAAEJuGAEAAAAAWg%2FD2xxY0C5LB7gBzCF0bHnn4gA%3DjieJ6vLSe2EgA3YIrhlaNvfHtCy7gr44rTa9wFK1MGhAbwdaz5'
 }
 
