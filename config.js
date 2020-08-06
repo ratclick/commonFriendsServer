20132f8757efc0ec52325c73d3c1f6b0948a60c3
Ayushman <ayushman@honeybadgerlabs.in>
2020-08-06 12:01:02 +0530

    removed token from config.js

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ var config = {}
 
 config.twitter = {
     'apiUrl': 'https://api.twitter.com/1.1/',
-    // 'token': 'AAAAAAAAAAAAAAAAAAAAAEJuGAEAAAAAWg%2FD2xxY0C5LB7gBzCF0bHnn4gA%3DjieJ6vLSe2EgA3YIrhlaNvfHtCy7gr44rTa9wFK1MGhAbwdaz5'
+    // add your token key here
+    // 'token': ''
 }
 
 module.exports = config;
