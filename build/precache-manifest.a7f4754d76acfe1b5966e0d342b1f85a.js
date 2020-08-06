dc2076831ac892f1c94436204b0f908c0c45fd1e
Ayushman <ayushman@honeybadgerlabs.in>
2020-08-06 12:48:19 +0530

    added build folder and removed node_modules from git tracking

---
+++ build/precache-manifest.a7f4754d76acfe1b5966e0d342b1f85a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6757ddcff8aad88aef0891fb2fce80b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d05b6f486a576459faef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "32ee0a5a79a6d83041d8",
+    "url": "/static/js/2.b59f0d39.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b59f0d39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05b6f486a576459faef",
+    "url": "/static/js/main.b1736aac.chunk.js"
+  },
+  {
+    "revision": "5d542fc5afb8169eceef",
+    "url": "/static/js/runtime-main.0813eb54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "116bea7712832bf2769cad5167d550a5",
+    "url": "/static/media/twitter_square_blue.116bea77.svg"
+  }
+]);
